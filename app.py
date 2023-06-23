60f1c8b409f40a030cab930693703046002ef584
cohenaj194 <cohenaj194@gmail.com>
2023-06-22 20:08:15 -0400

    its not great but it does work

--- app.py
+++ app.py
@@ -38,11 +38,10 @@ def scan():
         hq_only = request.form.get("hq_only")
         profit_amt = request.form.get("profit_amt")
         min_desired_avg_ppu = request.form.get("min_desired_avg_ppu")
-        # language = request.form.get("language")
         game_wide = request.form.get("game_wide")
         include_vendor = request.form.get("include_vendor")
         out_stock = request.form.get("out_stock")
-        filters = request.form.get("filters")
+        filters = [int(request.form.get("filters"))]
 
         # response = json.load(open("test.json"))
         headers = {'Accept': 'application/json',}
@@ -55,10 +54,11 @@ def scan():
             "min_sales": int(sale_amt),
             "hq": str_to_bool(hq_only),
             "home_server": home_server,
-            "filters": int(filters),
+            "filters": filters,
             "region_wide": str_to_bool(game_wide),
             "include_vendor": str_to_bool(include_vendor),
-            "show_out_stock": str_to_bool(out_stock)
+            "show_out_stock": str_to_bool(out_stock),
+            "universalis_list_uid": ""
         }
 
         # {
@@ -76,7 +76,7 @@ def scan():
         #     "show_out_stock": true
         # }
         print (json_data)
-        response = requests.post('http://api.saddlebagexchange.com/api/scan/', headers=headers, json=json_data).json()
+        response = requests.post('http://api.saddlebagexchange.com/api/scan/', headers=headers, json=json_data).json()["data"]
 
         print(response)
 
@@ -86,41 +86,25 @@ def scan():
 
         # change default order of table columns
         fieldnames = [
-            "real_name",
-            "ppu",
-            "home_server_price",
-            "profit_amount",
-            "sale_rates",
-            "avg_ppu",
-            "server",
             "ROI",
+            "avg_ppu",
+            "home_server_price",
+            "home_update_time",
+            "item_id",
+            "npc_vendor_info",
+            "ppu",
+            "profit_amount",
             "profit_raw_percent",
+            "real_name",
+            "sale_rates",
+            "server",
             "stack_size",
             "update_time",
-            "home_update_time",
             "url",
-            "npc_vendor_info",
-        ]
-        item_list = [
-            {
-                "real_name": item["real_name"],
-                "ppu": item["ppu"],
-                "home_server_price": item["home_server_price"],
-                "profit_amount": item["profit_amount"],
-                "sale_rates": item["sale_rates"],
-                "avg_ppu": item["avg_ppu"],
-                "server": item["server"],
-                "ROI": item["ROI"],
-                "profit_raw_percent": item["profit_raw_percent"],
-                "stack_size": item["stack_size"],
-                "update_time": item["update_time"],
-                "home_update_time": item["home_update_time"],
-                "url": item["url"],
-                "npc_vendor_info": item["npc_vendor_info"],
-            } for item in list(response.values())
         ]
 
-        return render_template('oldscan.html', results=item_list, fieldnames=fieldnames, len=len)
+
+        return render_template('oldscan.html', results=response, fieldnames=fieldnames, len=len)
 
 
 if __name__ == '__main__':
