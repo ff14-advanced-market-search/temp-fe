f53e987806a6eb516718ad2c22e1960b62de3dcf
cohenaj194 <cohenaj194@gmail.com>
2023-06-23 10:46:22 -0400

    adding region undercut

--- app.py
+++ app.py
@@ -20,6 +20,59 @@ def root():
     return render_template("index.html", len=len)
 
 
+@app.route("/scan", methods=["GET", "POST"])
+def scan():
+    if request.method == "GET":
+        return render_template("oldscan.html")
+    elif request.method == "POST":
+        scan_hours = request.form.get("scan_hours")
+        sale_amt = request.form.get("sale_amt")
+        roi = request.form.get("roi")
+        home_server = request.form.get("home_server")
+        stack_size = request.form.get("stack_size")
+        hq_only = request.form.get("hq_only")
+        profit_amt = request.form.get("profit_amt")
+        min_desired_avg_ppu = request.form.get("min_desired_avg_ppu")
+        game_wide = request.form.get("game_wide")
+        include_vendor = request.form.get("include_vendor")
+        out_stock = request.form.get("out_stock")
+        filters = [int(request.form.get("filters"))]
+
+        headers = {"Accept": "application/json"}
+        json_data = {
+            "preferred_roi": int(roi),
+            "min_profit_amount": int(profit_amt),
+            "min_desired_avg_ppu": int(min_desired_avg_ppu),
+            "min_stack_size": int(stack_size),
+            "hours_ago": int(scan_hours),
+            "min_sales": int(sale_amt),
+            "hq": str_to_bool(hq_only),
+            "home_server": home_server,
+            "filters": filters,
+            "region_wide": str_to_bool(game_wide),
+            "include_vendor": str_to_bool(include_vendor),
+            "show_out_stock": str_to_bool(out_stock),
+            "universalis_list_uid": "",
+        }
+
+        response = requests.post(
+            "http://api.saddlebagexchange.com/api/scan/",
+            headers=headers,
+            json=json_data,
+        ).json()
+
+        if "data" not in response:
+            return f"Error no matching data with given inputs {response}"
+        response = response["data"]
+
+        fieldnames = list(response[0].keys())
+
+        return render_template(
+            "oldscan.html", results=response, fieldnames=fieldnames, len=len
+        )
+
+
+
 @app.route("/uploadtimers", methods=["GET", "POST"])
 def uploadtimers():
     if request.method == "GET":
@@ -208,55 +261,34 @@ def petexport():
         )
 
 
-@app.route("/scan", methods=["GET", "POST"])
-def scan():
+@app.route("/regionundercut", methods=["GET", "POST"])
+def regionundercut():
     if request.method == "GET":
-        return render_template("oldscan.html")
+        return render_template("regionundercut.html")
     elif request.method == "POST":
-        scan_hours = request.form.get("scan_hours")
-        sale_amt = request.form.get("sale_amt")
-        roi = request.form.get("roi")
-        home_server = request.form.get("home_server")
-        stack_size = request.form.get("stack_size")
-        hq_only = request.form.get("hq_only")
-        profit_amt = request.form.get("profit_amt")
-        min_desired_avg_ppu = request.form.get("min_desired_avg_ppu")
-        game_wide = request.form.get("game_wide")
-        include_vendor = request.form.get("include_vendor")
-        out_stock = request.form.get("out_stock")
-        filters = [int(request.form.get("filters"))]
-
         headers = {"Accept": "application/json"}
+
+        addonData = request.form.get("addonData")
         json_data = {
-            "preferred_roi": int(roi),
-            "min_profit_amount": int(profit_amt),
-            "min_desired_avg_ppu": int(min_desired_avg_ppu),
-            "min_stack_size": int(stack_size),
-            "hours_ago": int(scan_hours),
-            "min_sales": int(sale_amt),
-            "hq": str_to_bool(hq_only),
-            "home_server": home_server,
-            "filters": filters,
-            "region_wide": str_to_bool(game_wide),
-            "include_vendor": str_to_bool(include_vendor),
-            "show_out_stock": str_to_bool(out_stock),
-            "universalis_list_uid": "",
+            "region": request.form.get("region"),
+            "homeRealmID":  76, #int(request.form.get("homeRealmID")),
+            "addonData": json.loads(addonData),
         }
 
         response = requests.post(
-            "http://api.saddlebagexchange.com/api/scan/",
+            "http://api.saddlebagexchange.com/api/wow/regionundercut",
             headers=headers,
             json=json_data,
         ).json()
 
-        if "data" not in response:
+        if "undercut_list" not in response:
             return f"Error no matching data with given inputs {response}"
-        response = response["data"]
+        response = response["undercut_list"]
 
         fieldnames = list(response[0].keys())
 
         return render_template(
-            "oldscan.html", results=response, fieldnames=fieldnames, len=len
+            "regionundercut.html", results=response, fieldnames=fieldnames, len=len
         )
 
 
