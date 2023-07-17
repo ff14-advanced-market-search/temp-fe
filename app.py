843af7c66bf35f30449934bd58595f0b0ec265ea
cohenaj194 <cohenaj194@gmail.com>
2023-07-16 22:19:14 -0400

    now working

--- app.py
+++ app.py
@@ -162,18 +162,73 @@ def ffxivcraftsim():
     elif request.method == "POST":
         headers = {"Accept": "application/json"}
 
-        json_data = json.loads(request.form.get("jsonData"))
+        json_data = {
+            "home_server": request.form.get("home_server"),
+            "cost_metric": request.form.get("cost_metric"),
+            "revenue_metric": request.form.get("revenue_metric"),
+            "sales_per_week": int(request.form.get("sales_per_week")),
+            "median_sale_price": int(request.form.get("median_sale_price")),
+            "job": int(request.form.get("job")),
+            "filters": [int(request.form.get("filters"))],
+            "stars": int(request.form.get("stars")),
+            "lvl": int(request.form.get("lvl")),
+            "yields": int(request.form.get("yields")),
+        }
 
-        response = requests.post(
-            "http://api.saddlebagexchange.com/api/pricecheck",
+        craftsim_post_json = requests.post(
+            "http://api.saddlebagexchange.com/api/recipelookup",
             headers=headers,
             json=json_data,
         ).json()
 
-        if "matching" not in response:
-            return "Error no matching data"
-        if len(response["matching"]) == 0:
-            return "Error no matching data"
+        craftsim_results = requests.post(
+            "http://api.saddlebagexchange.com/api/craftsim",
+            headers=headers,
+            json=craftsim_post_json,
+        ).json()["data"]
+
+        for item_data in craftsim_results:
+            del item_data["itemID"]
+            hq = item_data["hq"]
+            del item_data["hq"]
+            yields = item_data["yieldsPerCraft"]
+            del item_data["yieldsPerCraft"]
+
+            se_link = item_data["itemData"]
+            del item_data["itemData"]
+            universalisLink = item_data["universalisLink"]
+            del item_data["universalisLink"]
+
+            costEst = item_data["costEst"]
+            del item_data["costEst"]
+            revenueEst = item_data["revenueEst"]
+            del item_data["revenueEst"]
+
+            item_data["hq"] = hq
+            item_data["yields"] = yields
+            item_data["item-data"] = se_link
+            item_data["universalisLink"] = universalisLink
+
+            item_data["material_min_listing_cost"] = costEst[
+                "material_min_listing_cost"
+            ]
+            item_data["material_median_cost"] = costEst["material_median_cost"]
+            item_data["material_avg_cost"] = costEst["material_avg_cost"]
+
+            item_data["revenue_home_min_listing"] = revenueEst[
+                "revenue_home_min_listing"
+            ]
+            item_data["revenue_median"] = revenueEst["revenue_median"]
+            item_data["revenue_avg"] = revenueEst["revenue_avg"]
+
+        fieldnames = list(craftsim_results[0].keys())
+
+        return render_template(
+            "ffxiv_craftsim.html",
+            results=craftsim_results,
+            fieldnames=fieldnames,
+            len=len,
+        )
 
 
 @app.route("/uploadtimers", methods=["GET", "POST"])
