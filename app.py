d183bffbc12256e412a7f171dd683678e9c3e7be
cohenaj194 <cohenaj194@gmail.com>
2023-08-26 11:14:55 -0400

    add rate limit to temp flask app

--- app.py
+++ app.py
@@ -3,10 +3,14 @@ import json
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask_cors import CORS
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 import requests
 
 app = Flask(__name__)
-
+CORS(app)
+limiter = Limiter(get_remote_address, app=app, default_limits=["10 per second"])
 
 def str_to_bool(bool_str):
     if bool_str == "True":
@@ -30,56 +34,56 @@ def wow():
     return render_template("wow_index.html", len=len)
 
 
-@app.route("/scan", methods=["GET", "POST"])
-def scan():
-    if request.method == "GET":
-        return render_template("oldscan.html")
-    elif request.method == "POST":
-        scan_hours = request.form.get("scan_hours")
-        sale_amt = request.form.get("sale_amt")
-        roi = request.form.get("roi")
-        home_server = request.form.get("home_server")
-        stack_size = request.form.get("stack_size")
-        hq_only = request.form.get("hq_only")
-        profit_amt = request.form.get("profit_amt")
-        min_desired_avg_ppu = request.form.get("min_desired_avg_ppu")
-        game_wide = request.form.get("game_wide")
-        include_vendor = request.form.get("include_vendor")
-        out_stock = request.form.get("out_stock")
-        filters = [int(request.form.get("filters"))]
+# @app.route("/scan", methods=["GET", "POST"])
+# def scan():
+#     if request.method == "GET":
+#         return render_template("oldscan.html")
+#     elif request.method == "POST":
+#         scan_hours = request.form.get("scan_hours")
+#         sale_amt = request.form.get("sale_amt")
+#         roi = request.form.get("roi")
+#         home_server = request.form.get("home_server")
+#         stack_size = request.form.get("stack_size")
+#         hq_only = request.form.get("hq_only")
+#         profit_amt = request.form.get("profit_amt")
+#         min_desired_avg_ppu = request.form.get("min_desired_avg_ppu")
+#         game_wide = request.form.get("game_wide")
+#         include_vendor = request.form.get("include_vendor")
+#         out_stock = request.form.get("out_stock")
+#         filters = [int(request.form.get("filters"))]
 
-        headers = {"Accept": "application/json"}
-        json_data = {
-            "preferred_roi": int(roi),
-            "min_profit_amount": int(profit_amt),
-            "min_desired_avg_ppu": int(min_desired_avg_ppu),
-            "min_stack_size": int(stack_size),
-            "hours_ago": int(scan_hours),
-            "min_sales": int(sale_amt),
-            "hq": str_to_bool(hq_only),
-            "home_server": home_server,
-            "filters": filters,
-            "region_wide": str_to_bool(game_wide),
-            "include_vendor": str_to_bool(include_vendor),
-            "show_out_stock": str_to_bool(out_stock),
-            "universalis_list_uid": "",
-        }
+#         headers = {"Accept": "application/json"}
+#         json_data = {
+#             "preferred_roi": int(roi),
+#             "min_profit_amount": int(profit_amt),
+#             "min_desired_avg_ppu": int(min_desired_avg_ppu),
+#             "min_stack_size": int(stack_size),
+#             "hours_ago": int(scan_hours),
+#             "min_sales": int(sale_amt),
+#             "hq": str_to_bool(hq_only),
+#             "home_server": home_server,
+#             "filters": filters,
+#             "region_wide": str_to_bool(game_wide),
+#             "include_vendor": str_to_bool(include_vendor),
+#             "show_out_stock": str_to_bool(out_stock),
+#             "universalis_list_uid": "",
+#         }
 
-        response = requests.post(
-            "http://api.saddlebagexchange.com/api/scan/",
-            headers=headers,
-            json=json_data,
-        ).json()
+#         response = requests.post(
+#             "http://api.saddlebagexchange.com/api/scan/",
+#             headers=headers,
+#             json=json_data,
+#         ).json()
 
-        if "data" not in response:
-            return f"Error no matching data with given inputs {response}"
-        response = response["data"]
+#         if "data" not in response:
+#             return f"Error no matching data with given inputs {response}"
+#         response = response["data"]
 
-        fieldnames = list(response[0].keys())
+#         fieldnames = list(response[0].keys())
 
-        return render_template(
-            "oldscan.html", results=response, fieldnames=fieldnames, len=len
-        )
+#         return render_template(
+#             "oldscan.html", results=response, fieldnames=fieldnames, len=len
+#         )
 
 
 @app.route("/ffxiv_itemnames", methods=["GET", "POST"])
